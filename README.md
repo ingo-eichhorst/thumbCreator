c272750414da4fa3250f85e80751abfd5c186642
ingo-eichhorst <git@ingo-eichhorst.de>
2014-09-07 14:53:44 +0200

    v 0.2 - bugfixes and refacturing

--- README.md
+++ README.md
@@ -2,3 +2,63 @@ thumbCreator
 ============
 
 The script takes screenshots of the first movie in subdirectories of the current directory
+
+
+Version 0.2 - change log
+============
+
+1. backquote syntax (improvement)
+- changed syntax from "startDir=`pwd`" to "startDir=$(pwd)"
+- acceleration is expected
+
+2. mkdir (improvement)
+- create only a thumb folder if it does not already exists 
+
+3. variable names (improvement)
+- changed variable shema from camelCase to underscore_case
+- reason: someone very experienced told me and http://programmers.stackexchange.com/questions/27264/naming-conventions-camelcase-versus-underscore-case-what-are-your-thoughts-ab
+
+4. Changed folder-name (improvement)
+- changed thumb folder from "thumb" to "thumbs_<<resultion>>" this allows to run the script again if another resolution is needed.
+
+5. output Messages (improvement)
+- display messages and descriptions in the terminal
+- do not display output from avconv and ls
+
+6. path (improvement)
+- changed <<cd $path>> to <<cd "${path}">>
+- Reason: path could be with spaces
+
+
+
+
+open/ issues
+============
+
+ToDo: time handling with date instead of awk
+ToDo
+		Config
+		ich hab beinahe überall drin:
+		ME=$(basename $0)
+		ME_DIR=$(dirname $0)
+		[11:19:12] Christian Hartmann: und dann später:
+		# read config file if exists
+		ME_CONF=${ME_DIR}/config/${ME}.conf
+		[ -f ${ME_CONF} ] && source ${ME_CONF}
+		[11:19:43] Christian Hartmann: dann wird bei dir aus:  ### Configuration ###  ... das:   ### Default Configuration ###
+		[11:19:51] Christian Hartmann: default soll/muss sein
+		[11:20:09] Christian Hartmann: aber konfigurationen können auch wirklich konfiguriert werden
+
+
+ToDo
+		stil
+		[11:25:45] Christian Hartmann: auch noch guter stil:
+		xsltproc=$(which xsltproc)
+		und:
+		if [ -z "$xsltproc" ]
+		then
+		  echo "ERROR: xsltproc not available" >&2
+		  exit 9
+		fi
+
+
--- thumbCreator.sh
+++ thumbCreator.sh
@@ -2,7 +2,7 @@
 
 ### Info ### 
 # The script takes screenshots of the first movie in subdirectories of the current directory
-# It creates a directory /thumb in the subdirectory to save the images
+# It creates a directory /thumb_<<resulution>> in the subdirectory to save the images
 # Starting point, interval and resolution of the images can be defined
 
 ### Needed ### 
@@ -11,43 +11,53 @@
 ### Configuration ### 
 STARTSHOT=0 		      # timestamp of the first screenshot in sec.
 INTERVAL=30 		      # interval between the shots
-RESOLUTION="320x180" 	# Resolution of the output thumbnails
+RESOLUTION="640x360" 	# Resolution of the output thumbnails 320x180
 
-startDir=`pwd`
-echo $startDir
+echo "********** thumbCreator ***********"
+
+start_dir=$(pwd)
+echo "starting directory: "$start_dir
 
 # create an array of all sub-direcorys and loop through
 dirs=( $(find . -maxdepth 2 -type d -printf '%P\n') )
-echo ${#dirs[@]}
+echo "number of subdirectories: "${#dirs[@]}
 for path in "${dirs[@]}"
   do
-    echo $path
-    cd $path
+    echo ""
+    cd "${path}"
+    echo "******** subdirectory: "$path" ********"
       # Take the first matching file in the current directory
-      filename=`ls *.m* | head -1`
+      filename=$(ls *.m* | head -1)
       # perform only if there is any video file
       if [ -n "$filename" ]; then
-        # create subdir for saving the thumbs
-        mkdir thumb/
-        # Get movie length from file
-        ff=$(avconv -i "$filename" 2>&1)
-        d="${ff#*Duration: }"
-        length="${d%%,*}"
-        # convert time hh:mm:ss.mmm to ss.mmm
-        lengthSec=`echo $length | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'`
-        # convert time ss.mmm to ss
-        lengthSecInt=`echo $lengthSec | awk '{ split($0,a,"."); print a[1] }'`
-        # Set starting point for next movie
-        counter=$STARTSHOT
-        # Loop until end of movie is reached
-        while [ $counter -lt $lengthSecInt ] 	
-          do
-          # take a shot
-          avconv -an -ss $counter -t 0.01 -i "$filename" -s $RESOLUTION "thumb/$counter.jpg" 
-          #avconv: an=without audio; ss=start position; t=length; i=input; s=thumbnail size
-          # set counter to next shot position interval in sec.   
-          let counter=counter+INTERVAL		
-        done
+        # create subdir for saving the thumbs  
+        if [ ! -d ./thumbs_$RESOLUTION/ ]; then
+          mkdir thumbs_$RESOLUTION/
+          # Get movie length from file
+          ff=$(avconv -i "$filename" 2>&1)
+          d="${ff#*Duration: }"
+          length="${d%%,*}"
+          # convert time hh:mm:ss.mmm to ss.mmm
+          length_sec=$(echo $length | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+          # convert time ss.mmm to ss
+          length_sec_int=$(echo $length_sec | awk '{ split($0,a,"."); print a[1] }')
+          # Set starting point for next movie
+          counter=$STARTSHOT
+          # Loop until end of movie is reached
+          while [ $counter -lt $length_sec_int ] 	
+            do
+            # take a shot
+            avconv -an -ss $counter -t 0.01 -i "$filename" -s $RESOLUTION "thumbs_$RESOLUTION/$counter.jpg" >/dev/null 2>&1
+            #avconv: an=without audio; ss=start position; t=length; i=input; s=thumbnail size
+            # set counter to next shot position interval in sec.   
+            let counter=counter+INTERVAL
+          done
+          echo "$(tput setaf 2)Thumbs created!$(tput setaf 7)"
+        else
+          echo "$(tput setaf 3)Thumbs directory already exists$(tput setaf 7)"
+        fi
+      else
+        echo "$(tput setaf 1)No video files found in subdirectory$(tput setaf 7)"
       fi
-    cd $startDir
+    cd "${start_dir}"
   done
